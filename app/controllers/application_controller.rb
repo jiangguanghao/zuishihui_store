ab553e830a6df081a4ed93a4da2334b4f77c729b
jiangguanghao <yeyingsuifeng@gmail.com>
2019-10-10 09:54:22 +0800

    store 架构完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  def admin_requried
+  def admin_required
     if !current_user.admin?
       redirect_to "/", alert: "You are not admin."
     end
