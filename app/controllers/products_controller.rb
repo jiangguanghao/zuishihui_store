8a40d596aad0004f7c891496f3fbd11d6bfc4616
jiangguanghao <yeyingsuifeng@gmail.com>
2019-10-19 15:56:08 +0800

    empty cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,12 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     #code
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    current_cart.add_product_to_cart(@product)
+    # redirect_to root_path
+    redirect_back fallback_location: root_path
+    flash[:notice] = "加入购物车"
+  end
 end
