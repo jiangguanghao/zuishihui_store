6605f1518843c897ff8843686bf88b4cc10f24aa
jiangguanghao <yeyingsuifeng@gmail.com>
2019-10-21 10:17:11 +0800

    购物车搭建

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,9 +11,13 @@ class ProductsController < ApplicationController
 
   def add_to_cart
     @product = Product.find(params[:id])
-    current_cart.add_product_to_cart(@product)
-    # redirect_to root_path
-    redirect_back fallback_location: root_path
-    flash[:notice] = "加入购物车"
+    if !current_cart.products.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      # redirect_to root_path
+      flash[:notice] = "你已成功将#{@product.title}加入购物车"
+    else
+      flash[:warning] = "你的购物车内已有此物品"
+    end
+      redirect_back fallback_location: root_path
   end
 end
